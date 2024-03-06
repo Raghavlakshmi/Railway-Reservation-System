cc834c8d1b619ae6400758a35093a8cacaa3c89c
saiprasad <saiprasad7747@gmail.com>
2024-03-06 22:16:59 +0530

    Completed

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE HTML>
+<!--
+	Online Railway Reservation System
+	martdevelopers254 | @MartinMbithi
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Online Railway Reservation System</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="includes/css/main.css" />
+		<noscript><link rel="stylesheet" href="includes/css/noscript.css" /></noscript>
+
+		
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.php">Online Railway Reservation System</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+					
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="pass-login.php">Make Reservation</a></li>
+								<li><a href="employee/emp-login.php">Employee Login</a></li>
+								<li><a href="admin/emp-login.php">Admin Login</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"><span class="icon solid fa-train"></span></div>
+							<h2>Online Railway Reservation System</h2>
+							<p>Best, Simple, and User Friendly Way To Reserve Train Tickets Effectively</a></p>
+						</div>
+					</section>
+					<marquee behavior="scroll" id="marq"  scrollamount=10 direction="left" height="250px" onmouseover="nestop()" onmouseout="nestrt()">
+				<div>
+				<p><b>There is no proposal to extend to mail/express and superfast trains the flexi-fares currently applicable only to Intercity, Shatabdi and Vandhe Bharat trains,  said Railways Minister Suresh Prabhu.</b></p>
+				</br>
+				<p><b>The Railway ministry has posted the rate list on its Twitter account while asking people to lodge a complaint if they are overcharged.</b></p></br>
+				
+				
+				</div>
+			</marquee>
+			<!-- <div id="container" >
+			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://source.unsplash.com/1366x400/?train" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://source.unsplash.com/1366x400/?railwaystation" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://source.unsplash.com/1366x400/?vandebharat" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+			</div> -->
+
+			
+			
+			
+					<section id="footer">
+						<div class="inner">
+							<h2 class="major">Get in touch</h2>
+							
+							<ul class="contact">
+								<li class="icon solid fa-home">
+									Online Railway Reservation System<br />
+									PHP Project<br />
+									
+								</li>
+								<li class="icon solid fa-phone">9952642202</li>
+								<li class="icon solid fa-envelope"><a href="#">raghavlakshmi2004@gmail.com</a></li>
+								<li class="icon brands fa-twitter"><a href="#">twitter.com/Orrs</a></li>
+								<li class="icon brands fa-facebook-f"><a href="#">facebook.com/orrs</a></li>
+								<li class="icon brands fa-instagram"><a href="https://www.instagram.com/raghav_lakshmi_/">raghav_lakshmi_</a></li>
+							</ul>
+							
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="includes/js/jquery.min.js"></script>
+			<script src="includes/js/jquery.scrollex.min.js"></script>
+			<script src="includes/js/browser.min.js"></script>
+			<script src="includes/js/breakpoints.min.js"></script>
+			<script src="includes/js/util.js"></script>
+			<script src="includes/js/main.js"></script>
+
+	</body>
+</html>
